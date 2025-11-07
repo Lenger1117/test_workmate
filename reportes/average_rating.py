8fd6a7ae95e732577e91a94ad423c405db7d932e
lenger1117 <lenger1117@yandex.ru>
2025-11-07 11:18:27 +0300

    Исправлена синтаксическая ошибка в average_rating.py

--- reportes/average_rating.py
+++ reportes/average_rating.py
@@ -15,7 +15,7 @@ def generate_average_rating_report(data: List[Dict[str,str]]) -> List[Tuple[str,
     result = []
     for brand, ratings in brand_ratings.items():
         avg_r = sum(ratings) / len(ratings)
-        result.append(brand, avg_r)
+        result.append((brand, avg_r))
 
     result.sort(key=lambda x: (-x[1], x[0]))
     return result
